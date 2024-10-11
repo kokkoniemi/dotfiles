3d2656c9e3b18065a46a8f3d9914af95f0501ea3
kokkoniemi <mikko@kokkoniemi.com>
2024-10-11 22:59:18 +0300

    setup_bash.sh PS1 prompt

---
+++ setup_bash.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+bashrc_path="$HOME/.bashrc"
+if [ -e "$bashrc_path" ]; then
+	echo "'$bashrc_path' exists..."
+else
+	touch "$bashrc_path"
+	echo "Created '$bashrc_path'..."
+fi
+
+prompt_cmd_str="
+#Source git prompt
+. /usr/share/git-core/contrib/completion/git-prompt.sh
+
+# PS1
+
+update_PS1 () {
+	PS1=\"\W\[\e[91m\]\$(__git_ps1)\[\e[00m\]$ \"
+}
+
+update_PS1
+PROMPT_COMMAND=update_PS1
+"
+
+if ! grep -q "PROMPT_COMMAND=.*" "$bashrc_path"; then
+	echo "$prompt_cmd_str" >> "$bashrc_path"	
+	echo "Following written to $bashrc_path:"
+	echo "$prompt_cmd_str"
+else
+	echo "PROMPT_COMMAND already set"
+fi
+
